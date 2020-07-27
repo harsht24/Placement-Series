37b1669163f62ba73811ac918700f1dc37f417e6
Harsh Tamkiya <htamkiya24@gmail.com>
2020-07-27 17:21:24 +0530

    arraylist

---
+++ Data Structures in Java/ArrayList/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(22);
+        list.add(32);
+        list.add(42);
+        System.out.println(list);
+
+    }
+
+}
