87c290c1b01c6e93cd77d50817442e1edacdc5b0
Harsh Tamkiya <htamkiya24@gmail.com>
2020-07-29 09:50:21 +0530

    Create TwoSum

---
+++ Leetcode/TwoSum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int k = nums[0];
+        int result[] = new int[2];
+       Map<Integer,Integer> map = new HashMap<>();
+       for(int i=0; i<nums.length; i++){
+           int diff = target - nums[i];
+           
+           if (map.containsKey(diff)){
+               result[0] = i;
+               result[1] = map.get(diff);
+                   return result;
+           }
+           
+               map.put(nums[i],i);
+           
+       }
+        return result;
+    }
+}
