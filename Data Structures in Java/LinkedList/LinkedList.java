9029b81365667d8d6ce89e4704c118b9fe0a5003
Harsh Tamkiya <htamkiya24@gmail.com>
2020-07-30 22:51:24 +0530

    Create Linkedlist

---
+++ Data Structures in Java/LinkedList/LinkedList.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import org.w3c.dom.ls.LSInput;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Linkedlist {
+
+    public static void main(String[] args) {
+
+        LinkedList list = new LinkedList();
+        list.addFirst(23);
+        list.addLast(24);
+        list.add(25);
+        System.out.println(list);
+
+        System.out.println("Does the list contains 24? "+list.contains(24));
+
+        System.out.println("Index of 24 is : "+list.indexOf(24));
+
+        var arr = list.toArray(); // Convert linkedlist to array.
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
+
