94659ee74f91649c8fb433e5800c9ad6d44232df
Harsh Tamkiya <htamkiya24@gmail.com>
2020-09-14 10:11:03 +0530

    Create ArrangeBuildings.java

---
+++ Data Structures in Java/Foundation Java/Dynamic Programming/ArrangeBuildings.java
@@ -0,0 +1,27 @@
+// https://www.pepcoding.com/resources/online-java-foundation/dynamic-programming-and-greedy/arrange-buildings-official/ojquestion#
+
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int oldCountZeros = 1;
+        int oldCountOnes = 1;
+        
+        for(int i=2; i<=n; i++){
+            int newCountZeros = oldCountOnes;
+            int newCountOnes = oldCountOnes + oldCountZeros;
+            
+            oldCountZeros = newCountZeros;
+            oldCountOnes = newCountOnes;
+        }
+        int result = oldCountZeros + oldCountOnes;
+        System.out.println(result * result);
+ }
+
+}
