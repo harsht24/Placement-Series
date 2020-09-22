0114896c1cdf614470379e2033e95d939c112668
Harsh Tamkiya <htamkiya24@gmail.com>
2020-09-22 13:25:12 +0530

    Create DuplicateBrackets.java

---
+++ Data Structures in Java/Foundation Java/Stacks/DuplicateBrackets.java
@@ -0,0 +1,40 @@
+// https://www.pepcoding.com/resources/online-java-foundation/stacks-and-queues/duplicate-brackets-official/ojquestion#
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        // s = s.strip();
+        s = s.replaceAll("\\s+", "");
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++){
+            
+            char ch = s.charAt(i);
+            if(ch == ')'){
+                if(stack.peek() == '('){
+                    System.out.println(true);
+                    return;
+                }
+                else{
+                    while(stack.peek() != '(')
+                    {
+                    stack.pop();
+                    }
+                    stack.pop();
+                }
+            }
+            else{
+                stack.push(ch);   
+            }
+        }
+        
+            System.out.println(false);
+
+    }
+}
